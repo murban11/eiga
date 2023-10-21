1153f55c33cc3f87f00e53f853601b87168a2d96
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2023-10-21 13:35:54 +0200

    Add initial project configuration

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SimpleHeader />
+</template>
+
+<script>
+import SimpleHeader from './components/SimpleHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SimpleHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Eiga</h1>
+</template>
